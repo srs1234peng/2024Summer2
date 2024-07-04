47c1a19ff1090dbb08c43585262cd6007670235e
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-07-04 14:49:18 -0700

    add text input

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './component/Header';
+import React, { useState } from 'react'; // import useState
+import Input from './component/Input';
 
 export default function App() {
   const appName = "NewApp";
+  const [text, setText] = useState(''); // initialize text state
   return (
     <View style={styles.container}>
       {/* use a prop to pass appName to Header*/}
       <Header name = {appName} theme = "dark"></Header>
+      <Text>child1</Text>
+      <Text>child2</Text>
+      <Input></Input>
       <StatusBar style="auto" />
-      <Text>children</Text>
     </View>
   );
 }
