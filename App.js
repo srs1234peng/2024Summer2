9d5dd9dc8a1c95e1ac8e1e173d378714e6789c06
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-07-16 14:42:30 -0700

    added a warning button to goal details

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import Home from './component/Home';
+import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './component/Home';
 import GoalDetails from './component/GoalDetails';
 
 const Stack = createNativeStackNavigator();
-console.log(Stack);
 
 export default function App() {
   return (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} 
-      options={{
-        title:'My Awesome App', 
-        headerStyle:{backgroundColor:'dark purple',},
-        headerTintColor: '#fff', // Font color of the header
-            headerTitleStyle: {
-              fontWeight: 'bold',}
-              }}/>
-      <Stack.Screen name="GoalDetails" component={GoalDetails}/>
-    </Stack.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'My Awesome App',
+            headerStyle: { backgroundColor: 'darkmagenta' },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen
+          name="GoalDetails"
+          component={GoalDetails}
+          options={({ route }) => ({
+            title: route.params.goalObj.text,
+            headerRight: () => (
+              <Button
+                title="Warning"
+                onPress={() => alert('This is a warning message!')}
+                color="blue"
+              />
+            ),
+          })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
